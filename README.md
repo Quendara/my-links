f289fc8bed5720d0a7f9eefdb1fdfb9ad2d7278f
Quendara <andregarstka@web.de>
2020-02-26 23:27:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-links
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/my-links)
---
+++ index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+const items = [
+  {
+    name:"Tracking ",
+    link:"https://master.d1skuzk79uqu7w.amplifyapp.com/"
+  },
+  {
+    name:"Todos ",
+    link:"https://master.d3cslmw4si24vo.amplifyapp.com/"
+  },
+]
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Links' 
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+      <h3>Links</h3>
+      <div class="list-group">
+          { items.map(( item, index) => (
+            <a href={item.link} target="_blank" class="list-group-item  ">
+              <h5>{item.name}</h5>
+              <small class="text-muted">{item.link}</small> 
+              
+            </a>
+          ))}
+        
+      </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
