dbfe40428c7522c84d82faee3d1e09563154c075
Quendara <andregarstka@web.de>
2020-03-09 22:02:00 +0100

    Fix ids

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { List } from "./list";
 
 const items = [
   {
+    id: 1,
     name: "Tracking ",
     link: "https://master.d1skuzk79uqu7w.amplifyapp.com/"
   },
   {
+    id: 2,
     name: "Todos ",
     link: "https://master.d3cslmw4si24vo.amplifyapp.com/"
   },
   {
+    id: 3,
     name: "Bücherei ",
     link:
       "https://katalog.dortmund.de/aDISWeb/app?service=direct/0/Home/%24DirectLink&sp=SOPAC02&sp=SBK00000000"
